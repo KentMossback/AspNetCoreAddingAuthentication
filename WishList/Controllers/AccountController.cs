f6f2e2411f5c42ad9b3326d89c0d1a4bd99ae572
Kent Mossbäck <kent.mossback@scb.se>
2020-02-19 14:34:41 +0100

    No luck on checking the code

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -7,11 +7,10 @@ namespace WishList.Controllers
 {
     [Authorize]
     public class AccountController : Controller
-    {
+    {            
         private readonly UserManager<ApplicationUser> _userManager;
-
         private readonly SignInManager<ApplicationUser> _signInManager;
-
+        
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
